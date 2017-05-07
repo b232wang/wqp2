de3dff237241879031ed550965f3e3c74d6e1bba
Wang Boyuan <steven@v1020-wn-39-102.campus-dynamic.uwaterloo.ca>
2017-05-07 09:51:11 -0400

    1.0 done

---
+++ wqp2/Wqp2Main.java
@@ -0,0 +1,58 @@
+package wqp2;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Wqp2Main{
+
+    public static void main(String args[]) {
+        try{
+            Wqp2 wq = new Wqp2();
+
+            JFrame frame = new JFrame();
+            frame.setLayout(new GridLayout(1,1));
+            frame.setContentPane(wq);
+
+            wq.setFocusable(true);
+            wq.requestFocusInWindow();
+
+            frame.setSize(1200, 800);
+            frame.setVisible(true);
+
+        } catch (Exception e){
+            System.out.println("exception error");
+        }
+    }
+
+    //public static void main2(String args[]) throws Exception{
+    //    View v = new View();
+    //    Model m = new Model(v);
+    //    // m.view = v;
+    //    // v.model = m;
+    //    int ch;
+    //    System.out.print("enter: ");
+    //    while ((ch = System.in.read()) != -1){
+    //        int x = 0;
+    //        int y = 0;
+    //        if (ch != '\n' && ch != '\r'){
+    //            char c = (char)ch;
+    //            if(c == 'h'){
+    //                x = -1;
+    //            }
+    //            if(c == 'l'){
+    //                x = 1;
+    //            }
+    //            if(c == 'j'){
+    //                y = 1;
+    //            }
+    //            if(c == 'k'){
+    //                y = -1;
+    //            }
+    //            m.move(x, y);
+    //            System.out.print("enter: ");
+    //        }
+
+    //    }
+
+    //}
+}
